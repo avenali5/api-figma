b589d59b0cd43a8796d98a820fc8ded8df638399
Alejo Avenali <alejoavenali@gmail.com>
2023-12-26 15:42:50 -0300

    chango to mongo regular

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 // const searchRoute = require("./src/routes/searchRoute.js");
-const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 // Add Express
 const express = require("express");
 
 // Initialize Express
 const app = express();
 
+const mongoURI =
+  "mongodb+srv://alejinius:mikevallely@cluster0.nej8uwa.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Connect to MongoDB
+client.connect(err => {
+  if (err) {
+    console.error("Failed to connect to MongoDB:", err);
+  } else {
+    console.log("Connected to MongoDB");
+
+    // You can add your Express routes and other logic here
+
+    // Close the MongoDB connection when the app is closed
+    process.on("SIGINT", () => {
+      client.close(() => {
+        console.log("MongoDB connection closed");
+        process.exit(0);
+      });
+    });
+  }
+});
+
 // Create GET request
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
-
-const connectToDatabase = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://alejinius:mikevallely@cluster0.nej8uwa.mongodb.net/?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-};
-
-connectToDatabase();
 
 // Initialize server
 app.listen(5555, () => {
