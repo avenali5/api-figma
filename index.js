73a111d2f465d8a1c5be71caa7620f296387a6e3
Alejo Avenali <alejoavenali@gmail.com>
2023-12-26 15:11:29 -0300

    idk

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const productsRoute = require("./src/routes/productsRoute.js");
-const searchRoute = require("./src/routes/searchRoute.js");
+// const productsRoute = require("./src/routes/productsRoute.js");
+// const searchRoute = require("./src/routes/searchRoute.js");
 const serverless = require("serverless-http");
-const mongoose = require("mongoose");
-const { mongoDBURL } = require("./config.js");
+// const mongoose = require("mongoose");
+// const { mongoDBURL } = require("./config.js");
 // Add Express
 const express = require("express");
 
@@ -15,21 +15,21 @@ app.get("/", (req, res) => {
 });
 
 // Initialize server
-app.listen(5555, () => {
+app.listen(process.env.PORT || 5555, () => {
   console.log("Running on port 5555.");
 });
 
-app.use("/api/products", productsRoute);
-app.use("/api/search", searchRoute);
+// app.use("/api/products", productsRoute);
+// app.use("/api/search", searchRoute);
 
-mongoose
-  .connect(mongoDBURL)
-  .then(data => {
-    console.log("connected");
-  })
-  .catch(rtt => {
-    console.log(rtt);
-  });
+// mongoose
+//   .connect(mongoDBURL)
+//   .then(data => {
+//     console.log("connected");
+//   })
+//   .catch(rtt => {
+//     console.log(rtt);
+//   });
 
-module.exports = app;
+// module.exports = app;
 module.exports.handler = serverless(app);
