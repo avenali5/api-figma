4a50585b2043fdef08cd93c3402d6fc2b272dcc3
Alejo Avenali <alejoavenali@gmail.com>
2023-12-26 14:47:23 -0300

    fc

---
+++ index.js
@@ -0,0 +1,17 @@
+// Add Express
+const express = require("express");
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+// Initialize server
+app.listen(5555, () => {
+  console.log("Running on port 5555.");
+});
+
+module.exports = app;
